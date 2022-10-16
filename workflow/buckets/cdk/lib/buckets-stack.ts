cf76f8b60005299035325c7fa6f709a230f2667d
haruharuharuby <ito@hugtech.io>
2022-10-16 13:24:15 +0200

    add uploader

---
+++ workflow/buckets/cdk/lib/buckets-stack.ts
@@ -0,0 +1,54 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { PolicyStatement, Effect, AnyPrincipal } from 'aws-cdk-lib/aws-iam';
+
+export class BucketsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const uploadBucketName = new cdk.CfnParameter(this, 'UploadBucketName', {
+      type: 'String',
+      default: 'upload.sample-transcription-service',
+      // noEcho: true,
+    });
+
+    const websiteBucketName = new cdk.CfnParameter(this, 'WebsiteBucketName', {
+      type: 'String',
+      default: 'website.sample-transcription-service',
+      // noEcho: true,
+    });
+
+    const uploadBucket = new Bucket(this, 'uploadbucket', {
+      bucketName: uploadBucketName.valueAsString,
+      eventBridgeEnabled: true
+    })
+    const bucketPolicyUploadBucket = new PolicyStatement({
+      sid: `${uploadBucket.bucketName}-bucket-policy`,
+      effect: Effect.ALLOW,
+      actions: [
+        "s3:PutObject",
+        "s3:GetObject",
+      ],
+      principals: [new AnyPrincipal()],
+      resources: [uploadBucket.bucketArn + "/*"],
+    });
+    uploadBucket.addToResourcePolicy(bucketPolicyUploadBucket);
+
+    const uploadWebsiteBucket = new Bucket(this, 'webuploaderbucket', {
+      bucketName: websiteBucketName.valueAsString, 
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html'
+    })
+    const bucketPolicyWebsiteBucket = new PolicyStatement({
+      sid: `${uploadWebsiteBucket.bucketName}-bucket-policy`,
+      effect: Effect.ALLOW,
+      actions: [
+        "s3:GetObject",
+      ],
+      principals: [new AnyPrincipal()],
+      resources: [uploadWebsiteBucket.bucketArn + "/*"],
+    });
+    uploadWebsiteBucket.addToResourcePolicy(bucketPolicyWebsiteBucket);
+  }
+}
